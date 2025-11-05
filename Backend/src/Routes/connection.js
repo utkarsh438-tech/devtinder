48d8b0cec2d2c747dabcfd941cfe2fb1d2631405
utkarsh438-tech <utkarshtyagi437@gmail.com>
2025-11-05 14:37:19 +0530

    completed request routing

--- Backend/src/Routes/connection.js
+++ Backend/src/Routes/connection.js
@@ -38,4 +38,80 @@ ProfileRouter.post('/sendConnectionRequest', auth, async (req, res) => {
         res.status(500).send({ message: "Server error", error: error.message });
     }
 });
+ProfileRouter.post('/request/send/:status/:userId', auth, async (req, res) => {
+ try{
+    const fromUserId = req.user._id;
+    const toUserId = req.params.userId;
+    const status = req.params.status;
+
+
+    if (!['ignored','interested'].includes(status)) {
+        return res.status(400).send({ message: "Invalid status" });}
+
+
+        const toUser=await User.findById(toUserId);
+        if(!toUser){
+            return res.status(404).send({ message: "User not found" });
+        }
+    // const connectionRequest = await ConnectionRequest.findOne({ fromUserId, toUserId });
+    // if (!connectionRequest) {
+    //     return res.status(404).send({ message: "Connection request not found" });
+    // }
+
+
+    //if there is existing connection request, update it
+    const existingRequest = await ConnectionRequest.findOne({
+$or:[
+    { fromUserId, toUserId },
+    { fromUserId: toUserId, toUserId: fromUserId }
+] 
+         });
+    if( existingRequest ){
+        return res.status(400).send({ message:  "Connection request already exists" });
+        // res.json({ message: `Connection request ${status} successfully`, connectionRequest: existingRequest });
+    }
+
+
+
+    const connectionRequest=new ConnectionRequest({ fromUserId, toUserId, status });
+    await connectionRequest.save();
+    res.json({ message: `Connection request ${status} successfully`, connectionRequest });
+ }catch(error){
+    res.status(400).send({ message: "Server error", error: error.message });
+ }
+});
+
+
+ProfileRouter.post('/request/review/:status/:requestId', auth, async (req, res) => {
+    try{
+const LoggedInUserId=req.user._id;
+const requestId=req.params.requestId;
+const status=req.params.status;
+
+const allowedstatus=['accepted','rejected'];
+if(!allowedstatus.includes(status)){
+    return res.status(400).send({message:"Invalid status"});
+}
+const connectionRequest=await ConnectionRequest.findOne({
+    _id: requestId,
+    toUserId: LoggedInUserId,
+    status:'interested'
+
+});
+if(!connectionRequest){
+    return res.status(404).send({message:"Connection request not found"});
+}
+if(connectionRequest.status === 'accepted' || connectionRequest.status === 'rejected'){
+    return res.status(400).send({message:"Connection request already reviewed"});
+}
+connectionRequest.status=status;
+await connectionRequest.save();
+res.json({message:`Connection request ${status} successfully`,connectionRequest});
+
+
+
+    }catch(error){
+        res.status(400).send({message:"Server error",error:error.message});
+    }
+})
 module.exports = ProfileRouter;
--- Backend/src/models/connectionRequest.js
+++ Backend/src/models/connectionRequest.js
@@ -13,12 +13,23 @@ const connectionRequestSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['pending', 'accepted', 'rejected'],
+        enum: ['ignored', 'accepted', 'rejected','interested'],
         default: 'pending'
     }
+
+
 }, {
     timestamps: true
 });
+
+connectionRequestSchema.pre('save', function(next) {
+    if (this.fromUserId.toString() === this.toUserId.toString()) {
+        return next(new Error("Cannot send connection request to yourself"));
+    }
+
+    next();
+});
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 }, { unique: true });
 
 const ConnectionRequest = mongoose.model('ConnectionRequest', connectionRequestSchema);
 
